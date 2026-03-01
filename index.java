fa3035b128a30f0eb2c8258836a0302f98894ac2
ChristAmien <amienfabien@gmail.com>
2026-03-01 21:07:04 +0000

    premier commit du projet

---
+++ index.java
@@ -0,0 +1,9 @@
+public class index {
+    public static void main(String[] args) {
+        affichage("Marty");
+    }
+
+    public static void affichage (String nom){
+        System.out.println("Bonjour" + nom);
+    }
+}
