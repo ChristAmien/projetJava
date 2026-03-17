73f7f235efff904969ab79c5f52172f240294b6c
ChristAmien <amienfabien@gmail.com>
2026-03-17 10:10:40 +0000

    Delete index.java

--- index.java
+++
@@ -1,9 +0,0 @@
-public class index {
-    public static void main(String[] args) {
-        affichage("Marty");
-    }
-
-    public static void affichage (String nom){
-        System.out.println("Bonjour" + nom);
-    }
-}
